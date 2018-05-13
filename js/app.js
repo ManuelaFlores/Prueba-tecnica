d8987c3eee31ee1fad1b39f042d66f139c286461
Manuela <fmanuela499@gmail.com>
2018-05-12 23:32:44 -0500

    :recycle: refactoring code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function begin() {
-  // naming variables :
+  // naming variables:
   var firstSolution = document.querySelector('.solution-1-js');
   var secondSolution = document.querySelector('.solution-2-js');
   var highestNum = document.querySelector('.highest-number-js');
@@ -8,24 +8,23 @@ function begin() {
   var longestName = document.querySelector('.longest-name-js');
   var orderList = document.querySelector('.order-list');
   var arrayOfNumbers = [45, 75, 200, 256, 1, 27];
-  var arrayOfStrings =  ['erik', 'leonardo', 'andres', 'gerardo', 'jan', 'renzo'];
+  var arrayOfStrings = ['erik', 'leonardo', 'andres', 'gerardo', 'jan', 'renzo'];
 
-  //
+  // event handlers:
   function firstHandler() {
-   highestNum.textContent = highestNumber(arrayOfNumbers);
-   minimunNum.textContent = minimunNumber(arrayOfNumbers);
-   secondHighest.textContent = secondHighestNumber(arrayOfNumbers);
-  }
+    highestNum.textContent = highestNumber(arrayOfNumbers);
+    minimunNum.textContent = minimunNumber(arrayOfNumbers);
+    secondHighest.textContent = secondHighestNumber(arrayOfNumbers);
+  };
 
   function secondHandler() {
     longestName.textContent = getLongestName(arrayOfStrings);
     orderList.innerHTML = list(sorting(arrayOfStrings));
+  };
 
-  }
-
-  // adding event click to the buttons :
+  // adding event click to the buttons:
   firstSolution.addEventListener('click', firstHandler);
   secondSolution.addEventListener('click', secondHandler);
-}
+};
 
-window.addEventListener('load',begin)
+window.addEventListener('load', begin);
--- js/exercise-1.js
+++ js/exercise-1.js
@@ -3,32 +3,38 @@
 // a function that calculates the highest number of an array  :
 function highestNumber(collection) {
   var temp = -Infinity;
-  for (var i = collection.length; i >= 0; i--) {
+  var i = 0
+  for (; i < collection.length; i++) {
     collection[i] > temp ? temp = collection[i] : '';
   };
   return temp;
 };
 
 // a function that calculates the minimum number:
-
 function minimunNumber(collection) {
   var temp = Infinity;
-  for (var i = collection.length; i >= 0; i--) {
+  var i = 0;
+  for (; i < collection.length; i++) {
     collection[i] < temp ? temp = collection[i] : '';
   };
   return temp;
 };
 
 // a function that calculates the second highest number :
-
 function secondHighestNumber(collection) {
   var first = highestNumber(collection);
+  var counter = 0;
   var second = -Infinity;
-    for (var j = 0; j < collection.length; j++) {
-    collection[j] > second && collection[j] !== first ? second = collection[j] : '';
+  var i = 0;
+  for (; i < collection.length; i++) {
+    if (collection[i] > second && collection[i] === first) {
+      counter++;
+      if (counter === 2) {
+        second = collection[i];
+      };
+    } else if (collection[i] > second && collection[i] !== first) {
+      second = collection[i];
+    };
   };
   return second;
-};
-
-
-
+}
